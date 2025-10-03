693060f686703f3cbdd921c66bfc209725dc9b4e
akshayaamar <akshayaamar05@gmail.com>
2025-10-03 17:41:56 +0530

    One more example for RememberUpdatedState added

--- app/src/main/java/com/amar/jetpackcomposefirst/MainActivity.kt
+++ app/src/main/java/com/amar/jetpackcomposefirst/MainActivity.kt
@@ -32,7 +32,8 @@ class MainActivity : ComponentActivity() {
 //                         LaunchedEffectDemo()
 //                         RememberCoroutineScopeComposable()
 //                         RememberUpdatedStateDemo()
-                         RememberUpdatedStateAnotherExample()
+//                         RememberUpdatedStateAnotherExample()
+                         RememberUpdatedStateAnotherExampleThird()
                     }
                }
           }
--- app/src/main/java/com/amar/jetpackcomposefirst/RememberUpdatedStateScreen.kt
+++ app/src/main/java/com/amar/jetpackcomposefirst/RememberUpdatedStateScreen.kt
@@ -1,7 +1,14 @@
 package com.amar.jetpackcomposefirst
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -11,8 +18,58 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
+
+@Composable
+fun RememberUpdatedStateAnotherExampleThird() {
+     var buttonColor by remember { mutableStateOf("Not set") }
+
+     Column(
+          modifier = Modifier.fillMaxSize(),
+          horizontalAlignment = Alignment.CenterHorizontally,
+          verticalArrangement = Arrangement.Center
+     ) {
+          Button(
+               onClick = { buttonColor = "Magenta" },
+
+          ) {
+               Text(text = "Magenta Color")
+          }
+
+          Spacer(modifier = Modifier.height(48.dp))
+
+          Button(
+               onClick = { buttonColor = "Blue" },
+               colors = ButtonDefaults.buttonColors(Color.Blue)
+          ) {
+               Text(text = "Blue Color")
+          }
+
+          Timer(buttonColor)
+     }
+}
+
+@Composable
+fun Timer(buttonColor: String) {
+     val updateButtonColor by rememberUpdatedState(buttonColor)
+
+     LaunchedEffect(Unit) {
+          delay(5000)
+          Log.d("check...", "Button color is : $updateButtonColor")
+     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RememberUpdatedStateAnotherExampleThirdPreview() {
+     RememberUpdatedStateAnotherExampleThird()
+}
 
 fun first() {
      Log.d("check...", "This is first function")
